24e373c9f09b853519505140f9ae322daf3ddf99
Gustavo Barros <gbarrosfigueiredo@gmail.com>
2021-02-05 19:26:47 -0300

    desafio 1

---
+++ domain/model/user.go
@@ -0,0 +1,44 @@
+package model
+
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
+
+type User struct {
+	ID        string    `json:"id" valid:"uuid"`
+	Name      string    `json:"name" valid:"notnull"`
+	Email     string    `json:"email" valid:"notnull"`
+	CreatedAt time.Time `json:"created_at" valid:"-"`
+}
+
+func (user *User) isValid() error {
+	_, err := govalidator.ValidateStruct(user)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func NewUser(name string, email string) (*User, error) {
+	user := User{
+		Name:  name,
+		Email: email,
+	}
+
+	user.ID = uuid.NewV4().String()
+	user.CreatedAt = time.Now()
+
+	err := user.isValid()
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
